5c4786520666214c0e9135e00a13882416f81e41
Alex Paul <alex@alexpaul.net>
2011-02-17 16:32:37 -0800

    thinkpad migration

--- arch/install.sh
+++ arch/install.sh
@@ -1,9 +1,23 @@
 #!/bin/zsh
+
+# some prereqs
+pacman -S --needed --noconfirm vim git zsh sudo
+
 here=`dirname $0`
 
-pacman -Syua --noconfirm
+# rc.conf
+# fix hostname
+if [ $(hostname) = "myhost" ]; then
+    read "newname?Enter hostname:"
+    sed  -e "s/HOSTNAME='myhost'/HOSTNAME='$newname'" < /etc/rc.conf > /tmp/rc.conf
+    sudo mv /tmp/rc.conf /etc/rc.conf
+fi
+# fix daemons
+#TODO: 
+
+pacman -Syu --noconfirm
 pacman -S --needed --noconfirm yaourt
-installfile "$here/yaourtrc" "/etc/yaourtrc" 
+cp "$here/yaourtrc" "/etc/yaourtrc" 
 
 #TODO: generate packages.list from current install
 yaourt -S --noconfirm --needed --aur `cat $here/packages.list`
--- arch/makelist.sh
+++ arch/makelist.sh
@@ -1,24 +1,24 @@
 #!/bin/zsh
 
 rm -f packages.list ignore.list
-
 # figure out which groups to install
 yaourt -Qga --date > groups.list
 cat groups.list | while read group; do
     echo
     read -q "addgroup?Install group $group [y/N]: "
     if [ "$addgroup" = "y" ]; then
-        yaourt -Sg $group | grep -o '^[^ ]*' > $group.list
+        yaourt -Sg $group | grep -o '^[^ ]* ' > $group.list
         echo $group >> packages.list
         grep -o '[^/]*$' $group.list >> ignore.list
         rm -f $group.list
     fi
 done
 
-yaourt -Qea --date | grep -o '^[^ ]*' > explicit.list
-grep -v -F -f ignore.list explicit.list | grep -o '[^/]*$' >> packages.list
+yaourt -Qea --date | grep -o '^local/[^ ]* ' > aurpkgs.list
+yaourt -Qea --date | grep -o '^[^ ]* ' | grep -v '^local/' | grep -v -F -f ignore.list > explicit.list
+grep -o '[^/]* $' explicit.list >> packages.list
 
-rm -f ignore.list explicit.list groups.list
+rm -f ignore.list groups.list
 
 
 
